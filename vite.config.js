81e935b486a18bcfe559f13e36cdfbecd5f7bc9a
SunAndBoats <201173636+SunAndBoats@users.noreply.github.com>
2025-06-13 18:01:28 +1200

    redirect fix

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './tests/setup.js',
   },
 })
+
+
+
